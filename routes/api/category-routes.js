4105b413b4669cbdf3956fb78e9f57b2025afa56
Josh <lutz_143@yahoo.com>
2023-01-28 08:57:54 -0500

    added PUT request for a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,14 +27,18 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// router.get('/:id', (req, res) => {
-//   // find one category by its `id` value
-//   // be sure to include its associated Products
-// });
-
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
+
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
